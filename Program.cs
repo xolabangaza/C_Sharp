aaed01c3da28e980ed4e21a6abce1974770c8790
Xola Bangaza <xolabangaza@gmail.com>
2024-07-02 09:20:00 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BugSquasher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = 38498.3222;
+
+            string dinosaur = "Barney";
+
+            double lockV = 293.000;
+
+            bool isYes = true;
+
+            string band = "The Low Anthem";
+    
+    }
+    }
+}
